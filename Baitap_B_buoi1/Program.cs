94da2f1913477a51184346d5e6024a16d035662c
PHUOCMAX <phuocduong452006@gmail.com>
2026-09-15 10:40:04 +0700

    Bai4buoi1

---
+++ Baitap_B_buoi1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int t;
+
+        Console.Write("Nhap vao tong so giay: ");
+        t = int.Parse(Console.ReadLine());
+
+        int gio = t / 3600;
+        int phut = (t % 3600) / 60;
+        int giay = t % 60;
+
+        Console.WriteLine($"{t} giay co dang {gio:D2}:{phut:D2}:{giay:D2}");
+    }
+}
