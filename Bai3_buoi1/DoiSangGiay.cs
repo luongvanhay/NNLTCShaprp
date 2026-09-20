d7ecfd076f9f68eedac11648b33fb2b4bd6d3280
ngochiep3158 <phamhiep23032006@gmail.com>
2026-09-20 17:28:02 +0700

    nop bai 3 buoi 1

---
+++ Bai3_buoi1/DoiSangGiay.cs
@@ -0,0 +1,23 @@
+﻿// khai bao thu vien
+using System;
+class DoiSangGiay
+    {
+        public static void Main(string[] args)
+        {
+            // nhap du lieu 
+            Console.Write("nhap so gio:");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.Write("nhap so phut:");
+            int m = int.Parse(Console.ReadLine());
+            
+            Console.Write("nhap so giay:");
+            int s = int.Parse(Console.ReadLine());
+
+            // xy ly doi sang giay
+            int tonggiay = h * 3600 + m * 60 + s;
+
+            // xuat du lieu
+            Console.WriteLine($"tong so giay cua {h}:{m}:{s} la: {tonggiay} giay");
+        }
+    }
